78537f0b852ea84ea2d94eb978d66564327853d3
angelmc32 <angel.melendez.c@gmail.com>
2019-10-17 00:58:27 -0500

    Added medical consultation model :card_file_box:

---
+++ models/Consultation.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const consultationSchema = new Schema(
+  {
+    patient_id: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    provider_id: {
+      type: Schema.Types.ObjectId,
+      ref: 'Provider'
+    },
+    image_url: {
+      type: String,
+      required: true
+    },
+    chief_complaint: {
+      type: String,
+      enum: ['Cortada','Quemadura','Erupcion','Lunar','Hongo','Desconocido'],
+      required: true
+    },
+    symptoms: {
+      type: String,
+      enum: ['Ardor','Comezon','Dolor','Piel seca','Secreciones','Ampollas','Granos','Moreton','Sangrado']
+    },
+    pain_level: {
+      type: Number,
+      enum: [1,2,3,4,5,6,7,8,9,10]
+    },
+    description: {
+      type: String
+    },
+    start_date: {
+      type: Date
+    }
+  }
+)
