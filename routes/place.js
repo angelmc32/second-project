1bad867eb9cf552eabfd8b2c6b2354c9181d62cc
angelmc32 <angel.melendez.c@gmail.com>
2019-10-22 21:09:51 -0500

    :construction: modified map routes

--- routes/place.js
+++ routes/place.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const Place = require('../models/Place');
+const { isAuth, restrictAuth } = require('../helpers/authMiddleware');
 
 router.get('/places', (req,res) => {
-  res.render('places/index');
+  res.render('emergency/map');
 });
 
-router.get('/places/new', (req,res) => {
-  res.render('places/new');
+router.get('/places/new', isAuth, (req,res) => {
+
+  const { user } = req;
+
+  res.render('places/new', { title: 'Agregar clinica | Atencion a lesiones menores', user });
+
 });
 
 module.exports = router;
