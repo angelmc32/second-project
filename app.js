09ae26d90f794a4f94a5ae4cff3a89baa5c0516b
angelmc32 <angel.melendez.c@gmail.com>
2019-10-24 18:30:01 -0500

    :ambulance: modified connection to DB to use env variables

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const passport     = require('./helpers/passport');
 
 
 mongoose
-  .connect('mongodb://localhost/project-2', {useNewUrlParser: true, useUnifiedTopology: true})
+  .connect(process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
